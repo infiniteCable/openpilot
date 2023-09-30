71e7418cf1f1d40e58910da7b4b8af465727e875
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2023-09-30 16:24:27 +0200

    Update carstate.py: no ldw stock values available

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -92,7 +92,7 @@ class CarState(CarStateBase):
 
     # Consume factory LDW data relevant for factory SWA (Lane Change Assist)
     # and capture it for forwarding to the blind spot radar controller
-    self.ldw_stock_values = cam_cp.vl["LDW_02"] if self.CP.networkLocation == NetworkLocation.fwdCamera else {}
+    self.ldw_stock_values = {}
 
     # Stock FCW is considered active if the release bit for brake-jerk warning
     # is set. Stock AEB considered active if the partial braking or target
