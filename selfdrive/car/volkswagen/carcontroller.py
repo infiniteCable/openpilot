8b3131a2f6becaccdf27c5fd9350826cd9d29c0d
infiniteCable <werner.steffen@gmx.net>
2023-10-01 17:08:00 +0200

    revert coding and use the most aggressive/loudest ldw variant

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -72,9 +72,9 @@ class CarController:
 
     if self.frame % self.CCP.LDW_STEP == 0:
       hud_alert = 0
-      #if hud_control.visualAlert in (VisualAlert.steerRequired, VisualAlert.ldw):
-      hud_alert = self.CCP.LDW_MESSAGES["laneAssistTakeOver"]
-      can_sends.append(self.CCS.create_lka_hud_control(self.packer_pt, CANBUS.cam, CS.ldw_stock_values, True, #CC.enabled,
+      if hud_control.visualAlert in (VisualAlert.steerRequired, VisualAlert.ldw):
+        hud_alert = self.CCP.LDW_MESSAGES["laneAssistTakeOverUrgent"]
+      can_sends.append(self.CCS.create_lka_hud_control(self.packer_pt, CANBUS.cam, CS.ldw_stock_values, CC.enabled,
                                                        CS.out.steeringPressed, hud_alert, hud_control))
 
     # **** Stock ACC Button Controls **************************************** #
