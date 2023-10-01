0c1054bc7aa9b4c1418fdb107fab87e2948c29bb
infiniteCable <werner.steffen@gmx.net>
2023-10-01 19:08:59 +0200

    reenable steering fault detection

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -56,8 +56,8 @@ class CarState(CarStateBase):
 
     # Verify EPS readiness to accept steering commands
     hca_status = self.CCP.hca_status_values.get(pt_cp.vl["LH_EPS_03"]["EPS_HCA_Status"])
-    ret.steerFaultPermanent = False #hca_status in ("DISABLED", "FAULT")
-    ret.steerFaultTemporary = False #hca_status in ("INITIALIZING", "REJECTED")
+    ret.steerFaultPermanent = hca_status in ("DISABLED", "FAULT")
+    ret.steerFaultTemporary = hca_status in ("INITIALIZING", "REJECTED")
 
     # Update gas, brakes, and gearshift.
     ret.gas = pt_cp.vl["Motor_20"]["MO_Fahrpedalrohwert_01"] / 100.0
