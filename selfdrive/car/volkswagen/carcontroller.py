5957ebbc30fb41a94dadd8da7ba17a5578db8815
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2023-10-05 20:46:52 +0200

    Update carcontroller.py: revert test coding and remove accel again

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -25,35 +25,11 @@ class CarController:
     self.hca_frame_timer_running = 0
     self.hca_frame_same_torque = 0
     self.hca_steer_step = self.CCP.STEER_STEP_INACTIVE
-
-    self.code = 0
-    self.type = 0
-    self.acc_type = 0
-    self.hs = 1
     
   def update(self, CC, CS, ext_bus, now_nanos):
     actuators = CC.actuators
     hud_control = CC.hudControl
     can_sends = []
-
-    if self.frame % 400 == 0:
-      if self.code <= 2:
-        self.code = self.code + 1
-      else:
-        self.code = 0
-        if self.type <= 2:
-          self.type = self.type + 1
-        else:
-          self.type = 0
-          if self.acc_type <= 2:
-            self.acc_type = self.acc_type + 1
-          else:
-            self.acc_type = 0
-            if self.hs <= 0:
-              self.hs = self.hs + 1
-            else:
-              self.hs = 0
-
 
     # **** Steering Controls ************************************************ #
     
@@ -98,16 +74,6 @@ class CarController:
     else:
       self.hca_steer_step = self.CCP.STEER_STEP_INACTIVE
 
-    # **** Acceleration Controls ******************************************** #
-
-    if self.frame % self.CCP.ACC_CONTROL_STEP == 0 and self.CP.openpilotLongitudinalControl:
-      acc_control = self.CCS.acc_control_value(CS.out.cruiseState.available, CS.out.accFaulted, CC.longActive)
-      accel = clip(actuators.accel, self.CCP.ACCEL_MIN, self.CCP.ACCEL_MAX) if CC.longActive else 0
-      stopping = actuators.longControlState == LongCtrlState.stopping
-      starting = actuators.longControlState == LongCtrlState.starting
-      can_sends.extend(self.CCS.create_acc_accel_control(self.packer_pt, CANBUS.pt, self.acc_type, CC.longActive, accel,
-                                                         acc_control, stopping, starting, CS.esp_hold_confirmation))
-
     # **** HUD Controls ***************************************************** #
 
     if self.frame % self.CCP.LDW_STEP == 0:
@@ -131,11 +97,9 @@ class CarController:
 
     gra_send_ready = CS.gra_stock_values["COUNTER"] != self.gra_acc_counter_last
     if gra_send_ready:
-      #if self.CP.pcmCruise and (CC.cruiseControl.cancel or CC.cruiseControl.resume):
-      #  can_sends.append(self.CCS.create_acc_buttons_control(self.packer_pt, CANBUS.pt, CS.gra_stock_values,
-      #                                                       cancel=CC.cruiseControl.cancel, resume=CC.cruiseControl.resume))
-      #else:
-      can_sends.append(self.CCS.create_gra_buttons_control(self.packer_pt, CANBUS.pt, CS.gra_stock_values, self.code, self.type, self.hs))
+      if self.CP.pcmCruise and (CC.cruiseControl.cancel or CC.cruiseControl.resume):
+        can_sends.append(self.CCS.create_acc_buttons_control(self.packer_pt, CANBUS.pt, CS.gra_stock_values,
+                                                             cancel=CC.cruiseControl.cancel, resume=CC.cruiseControl.resume))
 
     new_actuators = actuators.copy()
     new_actuators.steer = self.apply_steer_last / self.CCP.STEER_MAX
