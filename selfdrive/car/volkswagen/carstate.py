7c2f92ee530da792b42ca691eb934e199c5e4e05
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2023-10-26 19:01:18 +0200

    Update carstate.py

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -148,7 +148,7 @@ class CarState(CarStateBase):
     self.upscale_lead_car_signal = bool(pt_cp.vl["Kombi_03"]["KBI_Variante"])
 
     #test = hex(int(cam_cp.vl["BAP_LDW_01"]["Stream"]))[2:]
-    self.bap_ldw_01 = self.bap.receive_can(0x17331901, hex(int(cam_cp.vl["BAP_LDW_01"]["Stream"])))
+    self.bap_ldw_01 = self.bap.receive_can(0x17331901, bytes.fromhex(hex(int(cam_cp.vl["BAP_LDW_01"]["Stream"]))[2:]))
     
     return ret
 
