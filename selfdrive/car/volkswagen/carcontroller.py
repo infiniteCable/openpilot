098c8914670bb125344a8c8d022ad9a45e369ee7
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2023-10-26 21:52:43 +0200

    Update carcontroller.py

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -166,21 +166,21 @@ class CarController:
     if bap_ldw_01 is not None:
       can_id, opcode, lsg_id, fct_id, bap_data = bap_ldw_01
 
-      if lsg_id == 25: # LDW
-        if opcode == 1: # get
-          if fct_id == 2: # configuration
-            can_frames = self.bap.send(bap_dest_hex, 0, lsg_id, fct_id, bytes.fromhex("030019000401"))
+      #if lsg_id == 25: # LDW
+      #  if opcode == 1: # get
+      #    if fct_id == 2: # configuration
+      #      can_frames = self.bap.send(bap_dest_hex, 0, lsg_id, fct_id, bytes.fromhex("030019000401"))
             #for (id, data) in can_frames:
             #  can_sends.append([bap_dest_hex, 0, data, CANBUS.cam])
               #can_sends.append(self.CCS.create_bap(self.packer_pt, CANBUS.cam, bap_dest, int.from_bytes(data)))
           
-          elif fct_id == 3: # functions
+          #elif fct_id == 3: # functions
             #can_frames = self.bap.send(bap_dest_hex, 4, lsg_id, fct_id, bytes.fromhex("3807E000040108003807E0"))
             #for (id, data) in can_frames:
             #  can_sends.append([bap_dest_hex, 0, data, CANBUS.cam])
               #can_sends.append(self.CCS.create_bap(self.packer_pt, CANBUS.cam, bap_dest, int.from_bytes(data)))
                             
-          elif fct_id == 1: # properties
+          #elif fct_id == 1: # properties
             #can_frames = self.bap.send(bap_dest_hex, 4, lsg_id, fct_id, bytes.fromhex("03001900040108003807E000000000000A00020001000200000000"))
             #for (id, data) in can_frames:
              # can_sends.append([bap_dest_hex, 0, data, CANBUS.cam])
