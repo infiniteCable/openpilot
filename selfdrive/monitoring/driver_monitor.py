670dea7e9cf485aeef5f662991a373541b15471a
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-02-28 20:21:53 +0100

    Update driver_monitor.py distracted time should be more aggressive! (no nerfing here)

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -22,9 +22,14 @@ class DRIVER_MONITOR_SETTINGS():
     self._AWARENESS_TIME = 30. # passive wheeltouch total timeout
     self._AWARENESS_PRE_TIME_TILL_TERMINAL = 15.
     self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
-    self._DISTRACTED_TIME = 11. # active monitoring total timeout
-    self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-    self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+    # self._DISTRACTED_TIME = 11. # active monitoring total timeout
+    # self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
+    # self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+
+    # MORE AGGRESSIVE PROMPTING!!
+    self._DISTRACTED_TIME = 5. # active monitoring total timeout
+    self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 2.
+    self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 4.
 
     self._FACE_THRESHOLD = 0.7
     self._EYE_THRESHOLD = 0.65
