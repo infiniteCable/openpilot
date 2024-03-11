a39be5843cd13de132ca46bd88a0e345db1510d6
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-03-11 17:28:27 +0100

    Update values.py

--- selfdrive/car/volkswagen/values.py
+++ selfdrive/car/volkswagen/values.py
@@ -134,7 +134,7 @@ class VolkswagenPQPlatformConfig(PlatformConfig):
 @dataclass(frozen=True, kw_only=True)
 class VolkswagenCarSpecs(CarSpecs):
   centerToFrontRatio: float = 0.45
-  steerRatio: float = 15.2
+  steerRatio: float = 15.5
 
 
 class Footnote(Enum):
