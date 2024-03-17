fbc1cf1c5a9abddbc744c68f1240b977600a9bbe
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-03-17 23:07:36 +0100

    Update carcontroller.py...............

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -45,7 +45,7 @@ class CarController(CarControllerBase):
 
     self.bap_ldw_mode = 0
 
-    self.sm = messaging.SubMaster(['longitudinalPlanIC'])
+    self.sm = messaging.SubMaster(['longitudinalPlan'])
     self.distance = 0.0
     self.safe_distance = 0.0
     self.lead_detected = False
@@ -55,10 +55,10 @@ class CarController(CarControllerBase):
     hud_control = CC.hudControl
     can_sends = []
     self.sm.update(0)
-    if self.sm.updated['longitudinalPlanIC']:
-      self.distance = self.sm['longitudinalPlanIC'].distance
-      self.safe_distance = self.sm['longitudinalPlanIC'].safeDistance
-      self.lead_detected = self.sm['longitudinalPlanIC'].leadDetected
+    if self.sm.updated['longitudinalPlan']:
+      self.distance = self.sm['longitudinalPlan'].aTargetMaxDEPRECATED
+      self.safe_distance = self.sm['longitudinalPlan'].aTargetMinDEPRECATED
+      self.lead_detected = self.sm['longitudinalPlan'].hasRightLaneDEPRECATED
 
     # **** Steering Controls ************************************************ #
 
