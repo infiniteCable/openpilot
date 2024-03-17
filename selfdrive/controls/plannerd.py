c098f0a808573db692eecd410d5d0741871127dd
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-03-17 20:45:24 +0100

    Update plannerd.py

--- selfdrive/controls/plannerd.py
+++ selfdrive/controls/plannerd.py
@@ -27,7 +27,7 @@ def plannerd_thread():
   cloudlog.info("plannerd got CarParams: %s", CP.carName)
 
   longitudinal_planner = LongitudinalPlanner(CP)
-  pm = messaging.PubMaster(['longitudinalPlan', 'uiPlan', 'CustomReserved0'])
+  pm = messaging.PubMaster(['longitudinalPlan', 'uiPlan', 'longitudinalPlanIC'])
   sm = messaging.SubMaster(['carControl', 'carState', 'controlsState', 'radarState', 'modelV2'],
                            poll='modelV2', ignore_avg_freq=['radarState'])
 
