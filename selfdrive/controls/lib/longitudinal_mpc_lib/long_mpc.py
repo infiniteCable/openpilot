96571c794a0e1be9b4f2a73a19af998b1fad9c1a
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-03-21 19:27:50 +0100

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -413,13 +413,13 @@ class LongitudinalMpc:
       self.lead_detected = False
       if any((lead_0_obstacle - get_safe_obstacle_distance(self.x_sol[:,1], t_follow))- self.x_sol[:,0] < 0.0):
         self.source = 'lead0'
-        self.lead_dist = lead_0_obstacle
+        self.lead_dist = lead_0_obstacle[0]
         #self.lead_safe_dist = max((get_safe_obstacle_distance(self.x_sol[:,1], t_follow))- self.x_sol[:,0])
         self.lead_detected = True
       if any((lead_1_obstacle - get_safe_obstacle_distance(self.x_sol[:,1], t_follow))- self.x_sol[:,0] < 0.0) and \
          (lead_1_obstacle[0] - lead_0_obstacle[0]):
         self.source = 'lead1'
-        self.lead_dist = lead_1_obstacle
+        self.lead_dist = lead_1_obstacle[0]
         #self.lead_safe_dist =  max((get_safe_obstacle_distance(self.x_sol[:,1], t_follow))- self.x_sol[:,0])
         self.lead_detected = True
 
