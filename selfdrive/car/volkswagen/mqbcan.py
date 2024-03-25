b9a96e29c73afcd0db0db8a35bfd2eefaf2aeda9
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-03-25 19:00:45 +0100

    Update mqbcan.py

--- selfdrive/car/volkswagen/mqbcan.py
+++ selfdrive/car/volkswagen/mqbcan.py
@@ -158,7 +158,8 @@ def create_acc_hud_control(packer, bus, acc_hud_status, set_speed, lead_distance
 
 def create_fcw_hud_control(packer, bus, fcw_hud_status):
   if fcw_hud_status == 1:
-    acc_warn = 2
+    acc_warn = 1
+    txt_dist_warn = 1
   elif fcw_hud_status == 2:
     acc_warn = 1
   else:
@@ -176,7 +177,7 @@ def create_fcw_hud_control(packer, bus, fcw_hud_status):
     "ACC_Tempolimit": 0,
     "ACC_Charisma_FahrPr": 0,
     "ACC_Charisma_Status": 0,
-    "ACC_Texte_Abstandswarner": 0,
+    "ACC_Texte_Abstandswarner": txt_dist_warn,
   }
     
   return packer.make_can_msg("ACC_04", bus, acc_04_values)
