68761cc540888e6e6340788a2d481b7514f8b9af
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-04-01 19:13:02 +0200

    Update values.py correct value for Polo again, let the torque learner do its work

--- selfdrive/car/volkswagen/values.py
+++ selfdrive/car/volkswagen/values.py
@@ -153,7 +153,7 @@ class VolkswagenPQPlatformConfig(PlatformConfig):
 @dataclass(frozen=True, kw_only=True)
 class VolkswagenCarSpecs(CarSpecs):
   centerToFrontRatio: float = 0.45
-  steerRatio: float = 15.1
+  steerRatio: float = 14.4 # VW Polo 6C
 
 
 class Footnote(Enum):
