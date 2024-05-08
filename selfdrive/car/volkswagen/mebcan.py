cc535fdef80c5c89e6c44ca190b8a922eedeb83b
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-05-08 18:00:52 +0200

    Update mebcan.py prepare for meb

--- selfdrive/car/volkswagen/mebcan.py
+++ selfdrive/car/volkswagen/mebcan.py
@@ -1,1 +1,31 @@
+def create_steering_control(packer, bus, apply_steer, lkas_enabled):
+  values = {
+    "HCA_01_LM_Offset": abs(apply_steer),
+    "HCA_01_Request": lkas_enabled,
+    "HCA_01_LM_OffSign": 1 if apply_steer < 0 and lkas_enabled == 1 else 0,
+    "HCA_01_Enable": lkas_enabled,
+    "HCA_01_Standby": not lkas_enabled,
+    "HCA_01_Available": 1,
+  }
+  return packer.make_can_msg("HCA_01", bus, values)
 
+
+def create_lka_hud_control(packer, bus, ldw_stock_values, lat_active, steering_pressed, hud_alert, hud_control):
+  values = {}
+  if len(ldw_stock_values):
+    values = {s: ldw_stock_values[s] for s in [
+      "LDW_SW_Warnung_links",   # Blind spot in warning mode on left side due to lane departure
+      "LDW_SW_Warnung_rechts",  # Blind spot in warning mode on right side due to lane departure
+      "LDW_Seite_DLCTLC",       # Direction of most likely lane departure (left or right)
+      "LDW_DLC",                # Lane departure, distance to line crossing
+      "LDW_TLC",                # Lane departure, time to line crossing
+    ]}
+
+  values.update({
+    "LDW_Status_LED_gelb": 1 if lat_active and steering_pressed else 0,
+    "LDW_Status_LED_gruen": 1 if lat_active and not steering_pressed else 0,
+    "LDW_Lernmodus_links": 3 if hud_control.leftLaneDepart else 1 + hud_control.leftLaneVisible,
+    "LDW_Lernmodus_rechts": 3 if hud_control.rightLaneDepart else 1 + hud_control.rightLaneVisible,
+    "LDW_Texte": hud_alert,
+  })
+  return packer.make_can_msg("LDW_02", bus, values)
