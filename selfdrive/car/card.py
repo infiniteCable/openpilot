206cb3c5cf099db3af9eddc3b1a8895f5b3fe1e7
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-06-27 18:42:32 +0200

    Update card.py

--- selfdrive/car/card.py
+++ selfdrive/car/card.py
@@ -67,7 +67,7 @@ class Car:
     self.CP.passive = not controller_available or self.CP.dashcamOnly
     if self.CP.passive:
       safety_config = car.CarParams.SafetyConfig.new_message()
-      safety_config.safetyModel = car.CarParams.SafetyModel.volkswagenMeb
+      safety_config.safetyModel = car.CarParams.SafetyModel.noOutput
       self.CP.safetyConfigs = [safety_config]
 
     # Write previous route's CarParams
