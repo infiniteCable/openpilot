30d36925545ce8cba283195095b1e432bea00a46
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-06-28 18:14:58 +0200

    Update carstate.py

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -340,9 +340,11 @@ class CarState(CarStateBase):
     ret.cruiseState.enabled     = self.acc_status in ("ACTIVE", "PRE_ACTIVE")
     ret.cruiseState.nonAdaptive = bool(pt_cp.vl["MEB_ACC_01"]["ACC_Limiter_Mode"])
     ret.cruiseState.standstill  = self.esp_hold_confirmation
-    ret.cruiseState.speed       = (cruiseSpeed1 + cruiseSpeed5) * CV.KPH_TO_MS
-    if ret.cruiseState.speed > 50: # settable maximum 180km/h
-      ret.cruiseState.speed = 0
+    
+    if self.CP.pcmCruise:
+      ret.cruiseState.speed = (cruiseSpeed1 + cruiseSpeed5) * CV.KPH_TO_MS
+      if ret.cruiseState.speed > 50: # settable maximum 180km/h
+        ret.cruiseState.speed = 0
 
     # Update button states for turn signals and ACC controls, capture all ACC button state/config for passthrough
     ret.leftBlinker = bool(pt_cp.vl["Blinkmodi_02"]["BM_links"])
