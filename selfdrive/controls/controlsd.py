87cb1c1f5054858bb61b56db890567c4748e5e14
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-06-28 18:45:35 +0200

    Update controlsd.py testing

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -273,8 +273,9 @@ class Controls:
     for i, pandaState in enumerate(self.sm['pandaStates']):
       # All pandas must match the list of safetyConfigs, and if outside this list, must be silent or noOutput
       if i < len(self.CP.safetyConfigs):
-        safety_mismatch = pandaState.safetyModel != self.CP.safetyConfigs[i].safetyModel or \
-                          pandaState.safetyParam != self.CP.safetyConfigs[i].safetyParam #or \
+        safety_mismatch = pandaState.alternativeExperience != self.CP.alternativeExperience
+                          #pandaState.safetyModel != self.CP.safetyConfigs[i].safetyModel or \
+                          #pandaState.safetyParam != self.CP.safetyConfigs[i].safetyParam or \
                           #pandaState.alternativeExperience != self.CP.alternativeExperience
       else:
         safety_mismatch = pandaState.safetyModel not in IGNORED_SAFETY_MODES
