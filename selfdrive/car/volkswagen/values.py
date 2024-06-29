1db2b1d857021adf865ad02146759ee198b8f944
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-06-29 17:02:03 +0200

    Update values.py

--- selfdrive/car/volkswagen/values.py
+++ selfdrive/car/volkswagen/values.py
@@ -78,7 +78,7 @@ class CarControllerParams:
       self.STEER_DRIVER_ALLOWANCE = 80    # Driver intervention threshold 0.8 Nm
       self.STEER_DELTA_UP = 4             # Max HCA reached in 1.50s (STEER_MAX / (50Hz * 1.50))
       self.STEER_DELTA_DOWN = 10          # Min HCA reached in 0.60s (STEER_MAX / (50Hz * 0.60))
-      self.STEER_MAX = 255                # Max heading control assist torque 3.00 Nm
+      self.STEER_MAX = 200                # Max heading control assist torque 3.00 Nm
 
       self.shifter_values = can_define.dv["Getriebe_11"]["GE_Fahrstufe"]
       self.hca_status_values = can_define.dv["LH_EPS_03"]["EPS_HCA_Status"]
