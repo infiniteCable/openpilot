e84ee282ea74f82616600790d7d199a5c7f4bd4d
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-07-11 19:52:17 +0200

    Update carcontroller.py

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -150,7 +150,8 @@ class CarController(CarControllerBase):
         self.long_active_prev = CC.longActive
         current_speed = CS.out.vEgo * CV.MS_TO_KPH
         reversing = True if CS.out.gearShifter in [car.CarState.GearShifter.reverse] else False
-        acc_control = self.CCS.acc_control_value(CS.out.cruiseState.available, CS.out.accFaulted, CC.longActive)
+        user_overriding = CS.out.gasPressed or CS.out.brakePressed
+        acc_control = self.CCS.acc_control_value(CS.out.cruiseState.available, CS.out.accFaulted, CC.longActive, user_overriding)
         can_sends.extend(self.CCS.create_acc_accel_control(self.packer_pt, CANBUS.pt, CS.acc_type, CC.longActive, accel,
                                                            acc_control, stopping, starting, CS.esp_hold_confirmation,
                                                            long_just_started, current_speed, reversing))
