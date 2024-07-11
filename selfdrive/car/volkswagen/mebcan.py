cedb178535de0138cc4bfc125f5b1823be94ce15
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-07-11 18:52:52 +0200

    Update mebcan.py

--- selfdrive/car/volkswagen/mebcan.py
+++ selfdrive/car/volkswagen/mebcan.py
@@ -82,13 +82,10 @@ def create_acc_accel_control(packer, bus, acc_type, acc_enabled, accel, acc_cont
   else:
     acc_hold_type = 0
 
-  if acc_enabled:
-    if stopping:
-      anhalteweg = 1
-    else:
-      anhalteweg = 20
+  if stopping:
+    anhalteweg = 1
   else:
-    anhalteweg = 0
+    anhalteweg = 20
   
   values = {
     "ACC_Typ": acc_type,
@@ -102,8 +99,8 @@ def create_acc_accel_control(packer, bus, acc_type, acc_enabled, accel, acc_cont
     "ACC_Anfahren": starting,
     "ACC_Anhalten": stopping,
     
-    "ACC_Anhalteweg": anhalteweg,
-    "ACC_Anforderung_HMS": acc_hold_type,
+    "ACC_Anhalteweg": anhalteweg if acc_enabled else 0,
+    "ACC_Anforderung_HMS": acc_hold_type if acc_enabled else 0,
     "Unknown_01": 25 if stopping else 31,
     "Unknown_02": 0 if stopping else 15,
     "ACC_Active": 0,
