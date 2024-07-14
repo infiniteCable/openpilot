3db2f4f3d9c9067039725962d98f35838b92702c
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-07-14 16:09:57 +0200

    Update carcontroller.py revert gra logic for long, we do have to fully understand acc signals and decouple ourself from them

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -36,9 +36,6 @@ class CarController(CarControllerBase):
     self.hca_frame_same_torque = 0
     self.lat_active_prev = False
     self.torque_wind_down = 0
-    self.long_active_prev = False
-    self.long_cancel = False
-    self.long_resume = False
 
   def update(self, CC, CS, now_nanos):
     actuators = CC.actuators
@@ -146,14 +143,6 @@ class CarController(CarControllerBase):
       starting = actuators.longControlState == LongCtrlState.pid and (CS.esp_hold_confirmation or CS.out.vEgo < self.CP.vEgoStopping)
      
       if self.CP.flags & VolkswagenFlags.MEB:
-        if CC.longActive and not self.long_active_prev:
-          self.long_active_prev = True
-          self.long_resume = True
-          
-        elif not CC.longActive and self.long_active_prev:
-          self.long_active_prev = False
-          self.long_cancel = True
-        
         acc_control = self.CCS.acc_control_value(CS.out.cruiseState.available, CS.out.accFaulted, CC.longActive)
         can_sends.extend(self.CCS.create_acc_accel_control(self.packer_pt, CANBUS.pt, CS.acc_type, CC.longActive, accel,
                                                            acc_control, stopping, starting, CS.esp_hold_confirmation,
@@ -196,21 +185,10 @@ class CarController(CarControllerBase):
 
     # **** Stock ACC Button Controls **************************************** #
 
-    gra_send_ready = CS.gra_stock_values["COUNTER"] != self.gra_acc_counter_last
-    if self.CP.pcmCruise:
-      if gra_send_ready and (CC.cruiseControl.cancel or CC.cruiseControl.resume):
-        can_sends.append(self.CCS.create_acc_buttons_control(self.packer_pt, self.ext_bus, CS.gra_stock_values,
-                                                             cancel=CC.cruiseControl.cancel, resume=CC.cruiseControl.resume))
-        
-    elif self.CP.openpilotLongitudinalControl and self.CP.flags & VolkswagenFlags.MEB:
-      # prevent car from faulting
-      # keep car cruise control snychron with OP control
-      if gra_send_ready and (self.long_cancel or self.long_resume):
-        can_sends.append(self.CCS.create_acc_buttons_control(self.packer_pt, CANBUS.pt, CS.gra_stock_values,
-                                                             cancel=self.long_cancel, resume=self.long_resume))
-        self.long_resume = False
-        self.long_cancel = False
-      
+    gra_send_ready = self.CP.pcmCruise and CS.gra_stock_values["COUNTER"] != self.gra_acc_counter_last
+    if gra_send_ready and (CC.cruiseControl.cancel or CC.cruiseControl.resume):
+      can_sends.append(self.CCS.create_acc_buttons_control(self.packer_pt, self.ext_bus, CS.gra_stock_values,
+                                                           cancel=CC.cruiseControl.cancel, resume=CC.cruiseControl.resume))
 
     new_actuators = actuators.as_builder()
     new_actuators.steer = self.apply_steer_last / self.CCP.STEER_MAX
