32cdf710e39ff26388868df154b724f5c92b67b5
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-07-14 11:12:49 +0200

    Update mebcan.py

--- selfdrive/car/volkswagen/mebcan.py
+++ selfdrive/car/volkswagen/mebcan.py
@@ -58,9 +58,11 @@ def create_acc_buttons_control(packer, bus, gra_stock_values, cancel=False, resu
   })
   return packer.make_can_msg("GRA_ACC_01", bus, values)
 
-def acc_control_value(main_switch_on, acc_faulted, long_active):
+def acc_control_value(main_switch_on, acc_faulted, long_active, acc_control_disable):
   if acc_faulted:
     acc_control = 6
+  elif acc_control_disable:
+    acc_control = 5
   elif long_active:
     acc_control = 3
   elif main_switch_on:
