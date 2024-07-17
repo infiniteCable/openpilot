70f9d3a6ad53a1c7e9421822e2cf9a27a47e9762
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-07-18 00:25:47 +0200

    Update interface.py

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -85,7 +85,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerActuatorDelay = 0.2
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
     elif ret.flags & VolkswagenFlags.MEB:
-      ret.steerActuatorDelay = 0.2
+      ret.steerActuatorDelay = 0.1
       #CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
     else:
       ret.steerActuatorDelay = 0.1
