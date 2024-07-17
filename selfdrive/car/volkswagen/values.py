1b059b1721902b111048b71ff7bf0d0e1661dd69
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-07-17 23:02:24 +0200

    Update values.py

--- selfdrive/car/volkswagen/values.py
+++ selfdrive/car/volkswagen/values.py
@@ -75,8 +75,9 @@ class CarControllerParams:
       self.LDW_STEP               = 10    # LDW_02 message frequency 10Hz
       self.ACC_HUD_STEP           = 6     # ACC_02 message frequency 16Hz
       self.STEER_DRIVER_ALLOWANCE = 80    # Driver intervention threshold 0.8 Nm
-      self.TORQUE_WIND_DOWN_MAX   = 125      
-      self.TORQUE_WIND_DOWN_MIN   = 20       
+      self.TORQUE_WIND_DOWN_MAX   = 125
+      self.TORQUE_WIND_DOWN_MIN   = 20
+      self.TORQUE_WIND_DOWN_USER  = 60
       self.ANGLE_MAX              = 360
       self.ANGLE_RATE_LIMIT_UP    = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[10., 1.6, .3])
       self.ANGLE_RATE_LIMIT_DOWN  = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[10., 7.0, 0.8])
