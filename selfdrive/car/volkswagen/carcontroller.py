2e1454f62a4cda4d49369451e40f97e30625434c
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-07-29 20:02:49 +0200

    Update carcontroller.py

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -12,7 +12,7 @@ VisualAlert = car.CarControl.HUDControl.VisualAlert
 LongCtrlState = car.CarControl.Actuators.LongControlState
 
 def apply_meb_curvature_limits(apply_curvature, apply_curvature_last, current_curvature, v_ego_raw, CCP):
-  if v_ego_raw > 9: # we don't enforce checks for this in panda at the moment, but keep it near measured current curv to project user input
+  if v_ego_raw > 1: # we don't enforce checks for this in panda at the moment, but keep it near measured current curv to project user input
     apply_curvature = clip(apply_curvature, current_curvature - CCP.CURVATURE_ERROR, current_curvature + CCP.CURVATURE_ERROR)
   apply_curvature = apply_std_steer_angle_limits(apply_curvature, apply_curvature_last, v_ego_raw, CCP)
   
