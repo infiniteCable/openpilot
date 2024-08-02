5f273729b673b5ac7434518bb0071b56bd3730e1
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-08-02 19:29:08 +0200

    Update carcontroller.py

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -202,9 +202,10 @@ class CarController(CarControllerBase):
 
         set_speed = hud_control.setSpeed * CV.MS_TO_KPH
         acc_hud_status = self.CCS.acc_hud_status_value(CS.out.cruiseState.available, CS.out.accFaulted, CC.enabled and CS.out.cruiseState.enabled, CC.cruiseControl.override)
-        can_sends.append(self.CCS.create_acc_hud_control(self.packer_pt, CANBUS.pt, acc_hud_status, set_speed, get_T_FOLLOW(hud_control.leadDistanceBars - 1),
-                                                         hud_control.leadVisible, hud_control.leadDistanceBars, self.long_heartbeat,
-                                                         CS.esp_hold_confirmation, CS.meb_acc_01_values, CS.distance_stock_values))
+        can_sends.append(self.CCS.create_acc_hud_control(self.packer_pt, CANBUS.pt, acc_hud_status, hud_control.setSpeed * CV.MS_TO_KPH,
+                                                         get_T_FOLLOW(hud_control.leadDistanceBars - 1), hud_control.leadVisible,
+                                                         hud_control.leadDistanceBars, self.long_heartbeat, CS.esp_hold_confirmation,
+                                                         CS.meb_acc_01_values, CS.distance_stock_values))
 
       else:
         lead_distance = 0
