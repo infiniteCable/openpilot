7e06d8e1834f31430ab870555dcb0652e810cd52
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-12-05 20:19:15 +0100

    Update latcontrol_curvature.py implement 3dof curv

--- selfdrive/controls/lib/latcontrol_curvature.py
+++ selfdrive/controls/lib/latcontrol_curvature.py
@@ -6,24 +6,25 @@ from openpilot.selfdrive.controls.lib.latcontrol import LatControl
 STEER_ANGLE_SATURATION_THRESHOLD = 2.5  # Degrees
 
 
-class LatControlAngle(LatControl):
+class LatControlCurvature(LatControl):
   def __init__(self, CP, CI):
     super().__init__(CP, CI)
     self.sat_check_min_speed = 5.
 
-  def update(self, active, CS, VM, params, steer_limited, desired_curvature, calibrated_pose):
-    angle_log = log.ControlsState.LateralAngleState.new_message()
+  def update(self, active, CS, VM, params, steer_limited, desired_curvature, calibrated_pose, modelV2):
+    curvature_log = log.ControlsState.LateralCurvatureState.new_message()
 
     if not active:
-      angle_log.active = False
-      angle_steers_des = float(CS.steeringAngleDeg)
+      curvature_log.active = False
+      curvature_desired = 0.0
     else:
-      angle_log.active = True
-      angle_steers_des = math.degrees(VM.get_steer_from_curvature(-desired_curvature, CS.vEgo, params.roll))
-      angle_steers_des += params.angleOffsetDeg
+      curvature_log.active = True
+      curvature_desired = VM.calc_curvature_3dof(modelV2, CS.latAccel, CS.longAccel, CS.yawRate, CS.vEgo, math.radians(CS.steeringAngleDeg))
 
+    angle_steers_des = math.degrees(VM.get_steer_from_curvature(-curvature_desired, CS.vEgo, params.roll))
+    angle_steers_des += params.angleOffsetDeg
     angle_control_saturated = abs(angle_steers_des - CS.steeringAngleDeg) > STEER_ANGLE_SATURATION_THRESHOLD
-    angle_log.saturated = self._check_saturation(angle_control_saturated, CS, False)
-    angle_log.steeringAngleDeg = float(CS.steeringAngleDeg)
-    angle_log.steeringAngleDesiredDeg = angle_steers_des
-    return 0, float(angle_steers_des), float(curvature_desired), curvature_log
+    curvature_log.saturated = self._check_saturation(angle_control_saturated, CS, False)
+
+    curvature_log.curvatureDesired = float(curvature_desired)
+    return 0, 0.0, float(curvature_desired), curvature_log
