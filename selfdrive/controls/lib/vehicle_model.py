f79b2764b585b9c09db72b13f132e6af556395c4
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-12-07 14:09:43 +0100

    Update vehicle_model.py

--- selfdrive/controls/lib/vehicle_model.py
+++ selfdrive/controls/lib/vehicle_model.py
@@ -20,7 +20,7 @@ from openpilot.common.simple_kalman import KF1D, get_kalman_gain
 from cereal import car, log
 
 ACCELERATION_DUE_TO_GRAVITY = 9.8
-CURVATURE_CORR_ALPHA_3DOF = 0.1
+CURVATURE_CORR_ALPHA_3DOF = 0.04
 
 
 class VehicleModel:
