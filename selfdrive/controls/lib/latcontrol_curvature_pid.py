20776b62b8ba2763c97f72690e12c7271848cc31
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-12-20 13:19:44 +0100

    Update latcontrol_curvature_pid.py simplification

--- selfdrive/controls/lib/latcontrol_curvature_pid.py
+++ selfdrive/controls/lib/latcontrol_curvature_pid.py
@@ -27,10 +27,9 @@ class LatControlCurvaturePID(LatControl):
       assert calibrated_pose is not None
       actual_curvature_pose = calibrated_pose.angular_velocity.yaw / CS.vEgo
       actual_curvature = interp(CS.vEgo, [2.0, 5.0], [actual_curvature_vm, actual_curvature_pose])
-
+      
       error = desired_curvature - actual_curvature
-      freeze_integrator = steer_limited or CS.steeringPressed or CS.vEgo < 5
-      output_curvature = self.pid.update(error, feedforward=desired_curvature, speed=CS.vEgo, freeze_integrator=freeze_integrator)
+      output_curvature = self.pid.update(error, feedforward=desired_curvature, speed=CS.vEgo)
 
       curvature_log.saturated = self._check_saturation(abs(desired_curvature - output_curvature) < 1e-5, CS, False)
       curvature_log.error = float(np.float32(error))
