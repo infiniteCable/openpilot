011d565df1bf2346a8df7c18536dd4d99d1f43f4
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-12-20 14:50:57 +0100

    Update latcontrol_curvature_pid.py

--- selfdrive/controls/lib/latcontrol_curvature_pid.py
+++ selfdrive/controls/lib/latcontrol_curvature_pid.py
@@ -27,7 +27,7 @@ class LatControlCurvaturePID(LatControl):
       roll_compensation = params.roll * ACCELERATION_DUE_TO_GRAVITY
       assert calibrated_pose is not None
       actual_curvature_pose = calibrated_pose.angular_velocity.yaw / CS.vEgo
-      actual_curvature = -interp(CS.vEgo, [2.0, 5.0], [actual_curvature_vm, actual_curvature_pose])
+      actual_curvature = interp(CS.vEgo, [2.0, 5.0], [actual_curvature_vm, actual_curvature_pose])
 
       gravity_adjusted_curvature = desired_curvature + (roll_compensation / (CS.vEgo ** 2))
       error = desired_curvature - actual_curvature
