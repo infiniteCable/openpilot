85839c09c6397c4926985fc839193e12f8d4c362
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-01-15 21:55:34 +0100

    Update latcontrol_curvature_pid.py introduce delayed steering compensation

--- selfdrive/controls/lib/latcontrol_curvature_pid.py
+++ selfdrive/controls/lib/latcontrol_curvature_pid.py
@@ -4,6 +4,7 @@ import numpy as np
 from cereal import log
 from openpilot.common.pid import PIDController
 from openpilot.common.numpy_fast import interp
+from openpilot.common.realtime import DT_MDL
 from openpilot.selfdrive.controls.lib.latcontrol import LatControl
 
 
@@ -15,6 +16,7 @@ class LatControlCurvaturePID(LatControl):
                              k_f=CP.lateralTuning.pid.kf, pos_limit=0.2, neg_limit=-0.2)
     self.kpBP = CP.lateralTuning.pid.kpBP
     self.kpV = CP.lateralTuning.pid.kpV
+    self.curvature_hist = deque([0.0], maxlen=int(round(CP.steerActuatorDelay / DT_MDL))+1)
 
   def update(self, active, CS, VM, params, steer_limited, desired_curvature, calibrated_pose):
     curvature_log = log.ControlsState.LateralCurvatureState.new_message()
@@ -22,6 +24,8 @@ class LatControlCurvaturePID(LatControl):
       output_curvature = 0.0
       curvature_log.active = False
       self.pid.reset()
+      self.curvature_hist.clear()
+      self.curvature_hist.append(0.0)
     else:
       curvature_log.active = True
       roll_compensation = -VM.roll_compensation(params.roll, CS.vEgo)
@@ -30,9 +34,13 @@ class LatControlCurvaturePID(LatControl):
       actual_curvature_3dof = -VM.calc_curvature_3dof(calibrated_pose.acceleration.y, calibrated_pose.acceleration.x, calibrated_pose.angular_velocity.yaw,
                                                       CS.vEgo, math.radians(CS.steeringAngleDeg), 0.)
       actual_curvature = interp(CS.vEgo, [2.0, 5.0], [actual_curvature_vm, actual_curvature_3dof])
+
+      self.curvature_hist.append(actual_curvature)
+      delayed_actual_curvature = self.curvature_hist[0]
+      
       roll_factor = 1 / (interp(CS.vEgo, self.kpBP, self.kpV) or 1)
 
-      error = desired_curvature - (actual_curvature + roll_compensation * roll_factor)
+      error = desired_curvature - (delayed_actual_curvature + roll_compensation * roll_factor)
       output_curvature = self.pid.update(error, feedforward=desired_curvature, speed=CS.vEgo)
 
       curvature_log.p = float(np.float32(self.pid.p))
