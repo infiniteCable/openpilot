ea266616175121985e839d027e07ecc798652916
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-01-16 19:28:22 +0100

    Update latcontrol_curvature_pid.py try to differentiate external and internal components (internal error, external disturbance)

--- selfdrive/controls/lib/latcontrol_curvature_pid.py
+++ selfdrive/controls/lib/latcontrol_curvature_pid.py
@@ -18,6 +18,20 @@ class LatControlCurvaturePID(LatControl):
     self.kpBP = CP.lateralTuning.pid.kpBP
     self.kpV = CP.lateralTuning.pid.kpV
     self.curvature_hist = deque([0.0], maxlen=int(round(CP.steerActuatorDelay / DT_CTRL))+1)
+    self.alpha_range = [0.15, 0.05]
+    self.v_alpha_range = [6., 28.]
+    self.lowpass_filtered = 0.0
+
+  def adaptive_alpha(self, u):
+    return interp(u, self.v_alpha_range, self.alpha_range)
+
+  def lowpass_filter(self, current_value, u):
+    alpha = self.adaptive_alpha(u)
+    self.lowpass_filtered = (1 - alpha) * self.lowpass_filtered + alpha * current_value
+    return self.lowpass_filtered
+
+  def highpass_filter(self, current_value, lowpass_value):
+    return current_value - lowpass_value
 
   def update(self, active, CS, VM, params, steer_limited, desired_curvature, calibrated_pose):
     curvature_log = log.ControlsState.LateralCurvatureState.new_message()
@@ -27,6 +41,7 @@ class LatControlCurvaturePID(LatControl):
       self.pid.reset()
       self.curvature_hist.clear()
       self.curvature_hist.append(0.0)
+      self.lowpass_filtered = 0.0
     else:
       curvature_log.active = True
       roll_compensation = -VM.roll_compensation(params.roll, CS.vEgo)
@@ -36,12 +51,15 @@ class LatControlCurvaturePID(LatControl):
                                                       CS.vEgo, math.radians(CS.steeringAngleDeg - params.angleOffsetDeg), 0.)
       actual_curvature = interp(CS.vEgo, [2.0, 5.0], [actual_curvature_vm, actual_curvature_3dof])
 
-      self.curvature_hist.append(actual_curvature)
-      delayed_actual_curvature = self.curvature_hist[0]
+      reaction = self.lowpass_filter(actual_curvature, CS.vEgo)
+      disturbance = self.highpass_filter(actual_curvature, reaction)
+
+      self.curvature_hist.append(reaction)
+      curvature_result = self.curvature_hist[0] + disturbance
       
       roll_factor = 1 / (interp(CS.vEgo, self.kpBP, self.kpV) or 1)
 
-      error = desired_curvature - (delayed_actual_curvature + roll_compensation * roll_factor)
+      error = desired_curvature - (curvature_result + roll_compensation * roll_factor)
       output_curvature = self.pid.update(error, feedforward=desired_curvature, speed=CS.vEgo)
 
       curvature_log.p = float(np.float32(self.pid.p))
