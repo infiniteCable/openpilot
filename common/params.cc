f6c8654dc36d556d4aeae27a563a06772d170793
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-01-23 17:08:35 +0100

    Revert "Update params.cc not satisfied"
    This reverts commit 15e82cd1e88363dd951e1f744649b40f66d68498.

--- common/params.cc
+++ common/params.cc
@@ -119,7 +119,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"DoShutdown", CLEAR_ON_MANAGER_START},
     {"DoUninstall", CLEAR_ON_MANAGER_START},
 	{"EnableScreenEvent", CLEAR_ON_MANAGER_START | CLEAR_ON_ONROAD_TRANSITION},
-    {"ExperimentalLongitudinalEnabled", PERSISTENT}, //| DEVELOPMENT_ONLY},
+    {"ExperimentalLongitudinalEnabled", PERSISTENT | DEVELOPMENT_ONLY},
     {"ExperimentalMode", PERSISTENT},
     {"ExperimentalModeConfirmed", PERSISTENT},
     {"FirmwareQueryDone", CLEAR_ON_MANAGER_START | CLEAR_ON_ONROAD_TRANSITION},
