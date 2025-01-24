a99a802002a825b31349b8b1f61b1fbdd1132567
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-01-24 19:02:03 +0100

    Update hud.h

--- selfdrive/ui/qt/onroad/hud.h
+++ selfdrive/ui/qt/onroad/hud.h
@@ -15,6 +15,7 @@ private:
   void drawSetSpeed(QPainter &p, const QRect &surface_rect);
   void drawCurrentSpeed(QPainter &p, const QRect &surface_rect);
   void drawText(QPainter &p, int x, int y, const QString &text, int alpha = 255);
+  void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity);
   void drawBatteryHeaterIcon(QPainter &p, const QRect &surface_rect);
 
   float speed = 0;
@@ -24,9 +25,7 @@ private:
   bool v_ego_cluster_seen = false;
   int status = STATUS_DISENGAGED;
   bool battery_heater_enabled = false;
-  QPixmap img_battery_heater_active;
+  QPixmap img_battery_heater_enabled;
   const int btn_size = 192;
   const int img_size = (btn_size / 4) * 3;
 };
-
-void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity);
