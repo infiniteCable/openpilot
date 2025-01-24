93fd901f70766a6cf34d74002d3d5bfac5d1da48
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-01-24 16:22:18 +0100

    Update hud.cc

--- selfdrive/ui/qt/onroad/hud.cc
+++ selfdrive/ui/qt/onroad/hud.cc
@@ -41,17 +41,6 @@ void HudRenderer::updateState(const UIState &s) {
   speed = std::max<float>(0.0f, v_ego * (is_metric ? MS_TO_KPH : MS_TO_MPH));
 }
 
-void drawIcon(QPainter &p, const QPoint &center, const QPixmap &img, const QBrush &bg, float opacity) {
-  p.setRenderHint(QPainter::Antialiasing);
-  p.setOpacity(1.0);  // bg dictates opacity of ellipse
-  p.setPen(Qt::NoPen);
-  p.setBrush(bg);
-  p.drawEllipse(center, btn_size / 2, btn_size / 2);
-  p.setOpacity(opacity);
-  p.drawPixmap(center - QPoint(img.width() / 2, img.height() / 2), img);
-  p.setOpacity(1.0);
-}
-
 void HudRenderer::draw(QPainter &p, const QRect &surface_rect) {
   p.save();
 
