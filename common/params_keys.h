b090a94ac8c7e3ddc8c2519e8c68703a3e0740b3
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2025-02-20 22:29:32 +0100

    add missing custom param

--- common/params_keys.h
+++ common/params_keys.h
@@ -12,6 +12,7 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"AthenadPid", PERSISTENT},
     {"AthenadUploadQueue", PERSISTENT},
     {"AthenadRecentlyViewedRoutes", PERSISTENT},
+	{"BatteryDetails", PERSISTENT},
     {"BootCount", PERSISTENT},
     {"CalibrationParams", PERSISTENT},
     {"CameraDebugExpGain", CLEAR_ON_MANAGER_START},
