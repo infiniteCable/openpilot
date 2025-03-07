988b31f391debc3980927e3dfb0d6a0530e67fb4
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-03-07 14:41:26 +0100

    Update params_keys.h

--- common/params_keys.h
+++ common/params_keys.h
@@ -34,6 +34,7 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"DisableUpdates", PERSISTENT},
     {"DisengageOnAccelerator", PERSISTENT},
     {"DongleId", PERSISTENT},
+    {"DontDisengageLatOnBrake", PERSISTENT},
     {"DoReboot", CLEAR_ON_MANAGER_START},
     {"DoShutdown", CLEAR_ON_MANAGER_START},
     {"DoUninstall", CLEAR_ON_MANAGER_START},
