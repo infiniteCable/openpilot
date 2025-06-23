1a7d7a6c1c0e1ef306e58143c3712788728b12ed
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-06-23 17:08:45 +0200

    Update drive_helpers.py

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -47,6 +47,12 @@ def get_speed_error(modelV2: log.ModelDataV2, v_ego: float) -> float:
     return float(vel_err)
   return 0.0
 
+def get_speed_from_plan(speeds, t_idxs, action_t=DT_MDL):
+  if len(speeds) == len(t_idxs):
+    v_target = np.interp(action_t, t_idxs, speeds)
+  else:
+    v_target = 0.0
+  return v_target
 
 def get_accel_from_plan(speeds, accels, t_idxs, action_t=DT_MDL, vEgoStopping=0.05):
   if len(speeds) == len(t_idxs):
